625076e0e72abb451665fa86b170bda577b8cb45
winkwink <amanda.winkler44@gmail.com>
2020-02-06 15:55:12 -0500

    progress made

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("hello worlds");
+
+
+function createCircle(radius){
+	return {
+	radius,
+	draw: function(){
+		console.log('draw');
+	}
+};
+
+}
+
+
+const circle = createCircle(1);
+
+// Constructor function
+function Circle (radius){
+	console.log('this', this);
+	this.radius = radius;
+	this.draw = function(){
+		console.log('draw');
+	}
+}
+
+const another = new Circle(1); 
+another.draw();
+
